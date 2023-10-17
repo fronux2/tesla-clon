c2091a3eace0d1fa8227ef1c862d965ca9b9c889
marcos <marcosantonio.munozg@gmail.com>
2023-10-17 16:24:15 -0300

    primer commit.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-7xl font-bold underline">
+        Hello world!
+      </h1>
+      </div>      
+    </>
+  )
+}
+
+export default App
